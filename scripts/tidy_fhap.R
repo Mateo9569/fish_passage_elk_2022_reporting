b3d1871d5cf5ff2d8c42cf51b00dc5dc76147f45
almac2022 <al@newgraphenvironment.com>
2023-02-24 08:12:30 -0800

    show work to build xref_fhap object

--- scripts/tidy_fhap.R
+++ scripts/tidy_fhap.R
@@ -2,21 +2,71 @@ library(tidyverse)
 library(sf)
 library(fpr)
 
-# process the fhap data and present in tables
+source('scripts/functions.R')
+
+
+# !!!! requires xref_fhap object in tables.R script
+
+# process the fhap data and present in tables.  This is just for tracking how we did it
+# fhap_raw <- readxl::read_excel(
+#   'data/FHAP.xlsx',
+#   skip = 2,
+#   col_names = F)
+#
+# col_names <- fhap_raw %>%
+#   wkb_col_names()
+#
+#
+# fhap_prep <- fhap_raw %>%
+#   slice(3:nrow(.)) %>%
+#   set_names(nm = col_names) %>%
+#   # pad our numbers below 3 digits to ease the join to the waypoints
+#   mutate(location_waypoint = stringr::str_pad(location_waypoint, 3, pad = "0")) %>%
+#   # this is off now but we used it to get started on mapping the types
+#   mutate(across(matches(c('_m', '_percent', 'cm', 'fractions')), as.numeric))
+#
+# # set the types - this is how we started but now we have a tibble
+# # get the column types as a tibble - might need to do this in two steps
+# types_tib <- tibble(type_readxl = sapply(fhap_prep, class)) %>%
+#   mutate(type_readxl = case_when(type_readxl == 'character' ~ 'text',
+#                           T ~ type_readxl))
+# # join to make xref object
+# xref <- bind_cols(
+#   names(fhap_prep) %>% enframe(name = NULL, value = 'spdsht'),
+#   types_tib
+# ) %>%
+#   mutate(report = stringr::str_replace_all(spdsht, '_', ' '),
+#          report = stringr::str_to_title(report),
+#          report = stringr::str_replace_all(report, 'M ', '(m)'),
+#          # just replace M if it is at the end of the string
+#          report = stringr::str_replace_all(report, 'M$', '(m)'),
+#          report = stringr::str_replace_all(report, 'Location', ''),
+#          report = stringr::str_replace_all(report, 'General', '')) %>%
+#   select(spdsht, report, type_readxl) %>%
+#   mutate(id_join = NA_integer_,
+#          id_side = NA_integer_)
 
 fhap_raw <- readxl::read_excel(
   'data/FHAP.xlsx',
-  skip = 2,
-  col_names = F)
+  skip = 3,
+  # need to leave the col names in at first so we select all the data
+  col_names = T,
+  col_types = xref_fhap %>% dplyr::pull(type_readxl)
+) %>%
+  purrr::set_names(nm = xref_fhap %>% pull(spdsht))
 
-col_names <- fhap_raw %>%
-  wkb_col_names()
+# qa the habitat unit length
+qa_hu_len <- left_join(
+  fhap_raw %>%
+    group_by(location_reach_number) %>%
+    summarise(len = max(location_distance_m)),
 
-fhap_prep <- fhap_raw %>%
-  slice(3:nrow(.)) %>%
-  set_names(nm = col_names) %>%
-  # pad our numbers below 3 digits to ease the join to the waypoints
-  mutate(location_waypoint = stringr::str_pad(location_waypoint, 3, pad = "0"))
+  fhap_raw %>%
+  group_by(location_reach_number) %>%
+  summarise(qa_len = sum(habitat_unit_length_m)),
+
+  by = 'location_reach_number'
+)
 
 
 # import the gps and join to the fhap
