861e5f1338e9a25378d7c57012c4c80cbbb08d8c
Mateo Winterscheidt <matwint45@gmail.com>
2023-02-28 12:58:54 -0800

    also closed #37

--- scripts/01_prep_data/0100-extract-inputs.R
+++ scripts/01_prep_data/0100-extract-inputs.R
@@ -886,19 +886,15 @@ hab_loc <- habitat_confirmations %>%
   mutate(survey_date = janitor::excel_numeric_to_date(as.numeric(survey_date)))
 
 ##add the species code
-hab_fish_codes <- habitat_confirmations %>%
-  purrr::pluck("species_by_common_name") %>%
-  select(-step) %>%
-  # Burbot species code was 'BT' which is bull trout, change it to correct one
-  mutate(species_code = case_when(common_name == 'Burbot' ~ 'BB', T ~ species_code))
+hab_fish_codes <- fishbc::freshwaterfish %>%
+  select(species_code = Code, common_name = CommonName) %>%
+  tibble::add_row(species_code = 'NFC', common_name = 'No Fish Caught')
 
 hab_fish_indiv_prep2 <- left_join(
   hab_fish_indiv_prep,
   hab_loc,
   by = 'reference_number'
-)  %>% mutate(
-  species = case_when(species == 'Fish Unidentified Species' ~ 'Unidentified Species',
-                      T ~ species))
+)
 
 
 hab_fish_indiv_prep3 <- left_join(
