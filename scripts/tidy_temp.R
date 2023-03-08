b65faa68e57e56c8d091982819c1f823eba5cfe9
almac2022 <al@newgraphenvironment.com>
2023-03-07 23:30:49 -0800

    change object names

--- scripts/tidy_temp.R
+++ scripts/tidy_temp.R
@@ -17,14 +17,14 @@ temp_raw <- readxl::read_excel('data/Weigert_Creek_Temp_Profile_CWF.xlsx',
          # day_of_week = as.numeric(strftime(date, "%w"))
   )
 
-
-sum_temp_prep <- temp_raw %>%
-  reframe(ave_temp = ave(temp), .by = week) %>%
+#------------summary by week-------------------
+temp_prep <- temp_raw %>%
+  reframe(ave_temp_h20 = ave(temp), .by = week) %>%
   distinct() %>%
-  mutate(ave_temp = round(ave_temp, 1))
+  mutate(ave_temp_h20 = round(ave_temp_h20, 1))
 
 #make a summary of the weeks days
-sum_weeks <- bind_rows(
+temp_week_prep <- bind_rows(
   temp_raw %>%
     select(date, week, day_of_week) %>%
     slice(1),
@@ -37,10 +37,22 @@ sum_weeks <- bind_rows(
   summarise(day_first = min(date),
             day_last = max(date))
 
-sum_temp_week <- left_join(
+temp_week <- left_join(
 
-  sum_weeks,
-  sum_temp_prep,
+  temp_week_prep,
+  temp_prep,
 
   by = 'week'
 )
+
+#------------celcius degree days--------------------
+temp_cdd <- temp_raw %>%
+  reframe(ave_dt = ave(temp), .by = date) %>%
+  distinct() %>%
+  mutate(ave_dt = round(ave_dt, 1)) %>%
+  filter(date > '2022-06-26' &
+           date < '2022-10-17' )  %>%
+  # group_by(day) %>%
+  summarize(cdd = sum(ave_dt))
+
+
